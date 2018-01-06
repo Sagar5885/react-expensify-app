9d24f668beeac85985041fcc3353b3a6b605dd43
Sagar Dodia <sdodia@walmartlabs.com>
2018-01-05 22:51:27 -0800

    Pass all test cases

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { AddExpansePage } from '../../components/AddExpansePage';
+import { AddExpensePage } from '../../components/AddExpansePage';
 import expenses from '../fixtures/expenses';
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 
 beforeEach(() => {
-  onSubmit = jest.fn();
+  addExpense = jest.fn();
   history = { push: jest.fn() };
-  wrapper = shallow(<AddExpansePage onSubmit={onSubmit} history={history} />);
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
 });
 
-test('should render AddExpansePage correctly', () => {
+test('should render AddExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
