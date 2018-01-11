1f85a5a97e31237cf436532f7364f73fe4cd1fa8
Sagar Dodia <sdodia@walmartlabs.com>
2018-01-10 16:11:01 -0800

    Add style for all

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter, {history} from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
@@ -10,35 +10,33 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
-let hasRender = false;
+let hasRendered = false;
 const renderApp = () => {
-  if(!hasRender){
+  if (!hasRendered) {
     ReactDOM.render(jsx, document.getElementById('app'));
-    hasRender = true;
+    hasRendered = true;
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
-      if(history.location.pathname === '/'){
+      if (history.location.pathname === '/') {
         history.push('/dashboard');
       }
     });
-    console.log('log in');
   } else {
     store.dispatch(logout());
     renderApp();
